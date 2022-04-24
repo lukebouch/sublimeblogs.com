b74e3d8802f7589b15a27802e641307c12e15638
Luke Bouch <lukebouch2014@gmail.com>
2022-04-24 16:36:28 -0400

    Add hero section

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,12 +1,57 @@
 @extends('_layouts.main')
 
 @section('body')
-<div class="fixed top-0 right-0 bottom-0 left-0 border-8 border-yellow pointer-events-none"></div>
-<div class="p-8 h-screen flex justify-center items-center">
-    <div class="text-center">
-        <img class="w-28 h-28 mx-auto mb-2" src="/assets/images/icon.png" alt="Sublime Blogs logo">
-        <h1>Sublime Blogs</h1>
-        <p class="text-lg">A headless CMS designed specifically for blogs.</p>
+    <div class="bg-gray-50">
+        <div class="relative overflow-hidden">
+            <div class="absolute inset-y-0 h-full w-full" aria-hidden="true">
+                <div class="relative h-full">
+                    <svg class="absolute right-full transform translate-y-1/3 translate-x-1/4 md:translate-y-1/2 sm:translate-x-1/2 lg:translate-x-full"
+                        width="404" height="784" fill="none" viewBox="0 0 404 784">
+                        <defs>
+                            <pattern id="e229dbec-10e9-49ee-8ec3-0286ca089edf" x="0" y="0" width="20" height="20"
+                                patternUnits="userSpaceOnUse">
+                                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                            </pattern>
+                        </defs>
+                        <rect width="404" height="784" fill="url(#e229dbec-10e9-49ee-8ec3-0286ca089edf)" />
+                    </svg>
+                    <svg class="absolute left-full transform -translate-y-3/4 -translate-x-1/4 sm:-translate-x-1/2 md:-translate-y-1/2 lg:-translate-x-3/4"
+                        width="404" height="784" fill="none" viewBox="0 0 404 784">
+                        <defs>
+                            <pattern id="d2a68204-c383-44b1-b99f-42ccff4e5365" x="0" y="0" width="20" height="20"
+                                patternUnits="userSpaceOnUse">
+                                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                            </pattern>
+                        </defs>
+                        <rect width="404" height="784" fill="url(#d2a68204-c383-44b1-b99f-42ccff4e5365)" />
+                    </svg>
+                </div>
+            </div>
+            <div class="relative pt-6 pb-16 sm:pb-24">
+                <div class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
+                    <div class="text-center">
+                        <h1 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl">
+                            <span class="block">Don't spin up a whole server</span>
+                            <span class="block text-primary-600">just for you blog.</span>
+                        </h1>
+                        <p class="mt-3 max-w-md mx-auto text-base text-gray-500 md:mt-5 md:text-lg md:max-w-3xl">
+                            Build out your site in the front-end of your choice and then hook it up to our api. Deploy it
+                            statically and have the peace of mind know your site can handle any traffic being thrown at it.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="relative pb-10">
+                <div class="absolute inset-0 flex flex-col" aria-hidden="true">
+                    <div class="flex-1"></div>
+                    <div class="flex-1 w-full bg-gray-800"></div>
+                </div>
+                <div class="max-w-7xl mx-auto px-4 sm:px-6">
+                    <img class="relative rounded-lg shadow-lg" src="/assets/images/post-editor.png"
+                        alt="The markdown editor for creating and editing posts.">
+                </div>
+            </div>
+        </div>
     </div>
-</div>
 @endsection
