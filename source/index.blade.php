624b6a84c772f09231250dac5208adb54221170b
Luke Bouch <lukebouch2014@gmail.com>
2022-04-25 21:15:03 -0400

    Fix headline typo

--- source/index.blade.php
+++ source/index.blade.php
@@ -32,7 +32,7 @@
                     <div class="text-center">
                         <h1 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl">
                             <span class="block">Don't spin up an entire CMS</span>
-                            <span class="block text-primary-600">just for you simple blog.</span>
+                            <span class="block text-primary-600">just for a simple blog.</span>
                         </h1>
                         <p class="mt-3 max-w-md mx-auto text-base text-gray-500 md:mt-5 md:text-lg md:max-w-3xl">
                             Pick your front-end, connect it to our api, and then deploy your site statically with ease. All
