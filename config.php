26f966c72d40865a2a4534302cf00d31f40c2bac
Luke Bouch <lukebouch2014@gmail.com>
2022-04-27 12:23:12 -0400

    Add newsletter sign up form

--- config.php
+++ config.php
@@ -3,6 +3,7 @@
 return [
     'production' => false,
     'baseUrl' => '',
+    'apiUrl' => 'https://api.sublime-blogs.test',
     'title' => 'Sublime Blogs',
     'description' => 'A headless CMS designed specifically for blogs.',
     'collections' => [],
