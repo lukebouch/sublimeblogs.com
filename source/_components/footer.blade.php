9ee2ee638929d104ab3784053c3d353648495854
Luke Bouch <luke@lukebouch.com>
2022-05-08 20:41:30 -0400

    Add support link

--- source/_components/footer.blade.php
+++ source/_components/footer.blade.php
@@ -1,8 +1,9 @@
 <footer class="bg-white">
     <div class="px-4 py-10 mx-auto overflow-hidden max-w-7xl sm:px-6 lg:px-8">
         <nav class="flex flex-wrap justify-center -mx-5 -my-2" aria-label="Footer">
-            <div class="px-5 py-2">
+            <div class="px-5 py-2 space-x-5">
                 <a href="/privacy-policy" class="text-base text-gray-500 hover:text-gray-900">Privacy Policy</a>
+                <a href="mailto:help@sublimeblogs.com" class="text-base text-gray-500 hover:text-gray-900">Support</a>
             </div>
         </nav>
         <p class="mt-8 text-base text-center text-gray-400">&copy; 2022 Sublime Blogs. All rights reserved.</p>
